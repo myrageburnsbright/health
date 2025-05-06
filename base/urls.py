caca65ffcaf97874ed7585a1a1d354ab09843564
lainvasora <myrageburnsbright@gmail.com>
2025-05-06 15:53:22 -0400

    checkout

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import index_view, service_detail_view
+from .views import index_view, checkout_view, service_detail_view, book_appointment
 
 app_name = "base"
 
 urlpatterns = [
-    path('', index_view, name='index'),
-    path('service/<service_id>', service_detail_view, name='service_detail'),
+    path("", index_view, name="index"),
+    path("service/<service_id>", service_detail_view, name="service_detail"),
+    path(
+        "book-appointment/<service_id>/<doctor_id>",
+        book_appointment,
+        name="book_appointment",
+    ),
+    path("checkout/<billing_id>", checkout_view, name="checkout"),
 ]
